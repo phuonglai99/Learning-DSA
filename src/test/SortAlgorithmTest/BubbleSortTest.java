7c8231706766bc52ce013b8bd473c10fb4a05d9b
phuonglai99 <plai34037@gmail.com>
2023-01-07 17:18:24 +0700

    add test

--- src/test/SortAlgorithmTest/BubbleSortTest.java
+++
@@ -1,8 +0,0 @@
-package test.SortAlgorithmTest;
-
-import org.junit.jupiter.api.Test;
-
-public class BubbleSortTest {
-    @Test
-    void testCase1(){}
-}
---
+++ src/test/java/Main.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    void test(){
+        System.out.println("hello");
+    }
+}
